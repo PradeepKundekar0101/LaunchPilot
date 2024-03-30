5a2582d57dbb2f7ad1f339453c365833dbb5c9ce
pradeepkundekar0101 <pradeepkundekargithub@gmail.com>
2024-03-30 10:46:09 +0530

    added redux toolkit

---
+++ apps/client/src/store/store.ts
@@ -0,0 +1,28 @@
+import {configureStore} from '@reduxjs/toolkit'
+import authReducer from './slices/authSlice'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer} from 'redux-persist'
+import { combineReducers } from '@reduxjs/toolkit'
+
+const persistConfig = {
+    storage:storage, 
+    key:"root",
+    version:1,
+}
+
+const reducer = combineReducers({ auth: authReducer })
+const persistedReducer = persistReducer(persistConfig,reducer)
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    devTools:true,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+})
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>
+
+ 
